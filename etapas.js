f13b36a22f52ec02896fbc94badb871327b510ab
Weltonolivr4 <welton.playsbr@gmail.com>
2022-05-18 12:30:25 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+    
+          titulo: 'VEREADOR',
+          numeros: 5,
+          candidatos: [
+                 {
+                  numero: '38111',
+                 nome: 'Fulano de Tal',
+                 partido: 'ABC'
+        
+        
+                  },
+                    {
+                   numero: '77222',
+                   nome: 'Beltrano da Silva',
+                   partido: 'DEFG',
+                  },
+              
+            ]
+
+    },
+    {
+        titulo: 'prefeito',
+        numeros: 2,
+        candidatos:[
+           {
+
+            numero: '99',
+            nome: 'Ciclano',
+            partido: 'ABC',    
+             vice: 'Cic',
+  
+          },
+             {
+             numero:'84',
+             nome: 'Zulano',
+             parito: 'QWERTY',
+             vice: 'Zul',
+             }
+        ]         
+    },
+]
+
+    
+
+    
+    
